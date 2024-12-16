25533af11a460d80ccaa94f4a48b74b414f2538f
Georgi Kunchev <georgi.kunchev@ict.eu>
2024-12-16 12:51:36 +0200

    Add models

--- demo/example/models.py
+++ demo/example/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.DO_NOTHING)
